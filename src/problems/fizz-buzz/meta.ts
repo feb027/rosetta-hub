f81e04db1eea6b476426b31eb5be58e157d30253
Feb07 <febnawanrochman2@gmail.com>
2025-11-14 08:49:17 +0700

    content: add fizzbuzz problem

---
+++ src/problems/fizz-buzz/meta.ts
@@ -0,0 +1,11 @@
+import type { ProblemMeta } from '../../types/problem';
+
+export const meta: ProblemMeta = {
+  title: 'FizzBuzz',
+  slug: 'fizz-buzz',
+  difficulty: 'easy',
+  tags: ['algorithm', 'math'],
+  description:
+    'Print numbers from 1 to 100. For multiples of 3, print "Fizz". For multiples of 5, print "Buzz". For multiples of both, print "FizzBuzz".',
+  createdAt: '2025-11-14',
+};
